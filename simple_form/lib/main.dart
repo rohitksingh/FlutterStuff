6d8dacc03492a7cc343109d99780c2475ac7a48c
Rohit <r4rohit002@gmail.com>
2019-09-28 02:52:38 -0700

    Customized Apperance

--- simple_form/lib/main.dart
+++ simple_form/lib/main.dart
@@ -4,15 +4,29 @@ void main() => runApp(MaterialApp(
 
   home: Scaffold(
     appBar: AppBar(
-      title: Text('This is Appbar',
+      title: Text(
+        'This is Appbar',
       ),
       centerTitle: true,
+      backgroundColor: Colors.green[400],
     ),
     body: Center(
-      child: Text('Hello Flutter'),
+      child: Text(
+        'Hello Flutter',
+        style: TextStyle(
+          color: Colors.green,
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+
     ),
     floatingActionButton: FloatingActionButton(
-       child: Text('Click'),
+      child: Text('Click'),
+      backgroundColor: Colors.green,
+      onPressed: (){
+        // some function
+      },
     ),
   ),
 
