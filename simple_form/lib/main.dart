4306ff10688b0fed45ba6b10482e88d8061a8ae6
Rohit <r4rohit002@gmail.com>
2019-09-28 12:44:29 -0700

    Added Stateless Widget to test Hot reload

--- simple_form/lib/main.dart
+++ simple_form/lib/main.dart
@@ -2,33 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
 
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text(
-        'This is Appbar',
-      ),
-      centerTitle: true,
-      backgroundColor: Colors.green[400],
-    ),
-    body: Center(
-      child: Text(
-        'Hello Flutter',
-        style: TextStyle(
-          color: Colors.green,
-          fontSize: 30.0,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Indie'
-        ),
-      ),
-
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('Click'),
-      backgroundColor: Colors.green,
-      onPressed: (){
-        // some function
-      },
-    ),
-  ),
+  home: HomePage(),
 
 ));
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'This is Appbar',
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green[400],
+      ),
+      body: Center(
+        child: Text(
+          'Hello Flutter',
+          style: TextStyle(
+              color: Colors.green,
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Indie'
+          ),
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+        backgroundColor: Colors.green,
+        onPressed: (){
+          // some function
+        },
+      ),
+    );
+  }
+}
+
