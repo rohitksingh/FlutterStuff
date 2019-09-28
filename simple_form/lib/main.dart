68d63deff616001c98c679556f90c1a524f99cff
Rohit <r4rohit002@gmail.com>
2019-09-28 02:36:07 -0700

    Added Body, FAB

--- simple_form/lib/main.dart
+++ simple_form/lib/main.dart
@@ -4,8 +4,15 @@ void main() => runApp(MaterialApp(
 
   home: Scaffold(
     appBar: AppBar(
-      title: Text('This is AppBar'),
+      title: Text('This is Appbar',
+      ),
       centerTitle: true,
+    ),
+    body: Center(
+      child: Text('Hello Flutter'),
+    ),
+    floatingActionButton: FloatingActionButton(
+       child: Text('Click'),
     ),
   ),
 
