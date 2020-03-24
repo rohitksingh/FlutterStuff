643e9163f09e115e7686826f81c739de3d1bf31a
Rohit <r4rohit002@gmail.com>
2020-03-23 21:39:01 -0700

    Initial Commit

---
+++ building_layouts/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  title: "Building layout Demo",
+  home: Scaffold(
+    appBar: AppBar(title: Text("Building Layouut"),),
+    body: Center(child: Text("This is title"),),
+  ),
+));
+
+
