e5071dd768ca35428a9e78536e774f1577fdcaf1
Rohit <r4rohit002@gmail.com>
2020-03-24 13:32:52 -0700

    Created title component

--- building_layouts/lib/main.dart
+++ building_layouts/lib/main.dart
@@ -4,8 +4,43 @@ void main() => runApp(MaterialApp(
   title: "Building layout Demo",
   home: Scaffold(
     appBar: AppBar(title: Text("Building Layouut"),),
-    body: Center(child: Text("This is title"),),
+    body: Column(
+      children: [
+        titleSection,
+      ],
+    ),
   ),
 ));
+
+
+Widget titleSection = Container(
+  padding: EdgeInsets.all(16),
+  color: Colors.amber,
+  child: Row(
+    children: [
+      Expanded(
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("This is the heading",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            Text("This is description")
+          ],
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(
+          Icons.mail,
+          color: Colors.red,
+        ),
+      ),
+      Text('41')
+    ],
+  ),
+);
 
 
